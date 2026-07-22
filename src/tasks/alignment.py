1e1d263adffcb56528c5c0dcd6299437dceba1dd
Takumi Sugimoto <ts20030211@gmail.com>
2026-07-22 20:10:52 +0900

    revert cost based alignment

--- src/tasks/alignment.py
+++ src/tasks/alignment.py
@@ -2,11 +2,13 @@ import os
 import shutil
 import tempfile
 
+import cv2
 import luigi
 import numpy as np
 import open3d
+import open3d as o3d
 import pycolmap
-import cv2
+import scipy
 
 import context
 import tasks.depth
@@ -67,6 +69,21 @@ class AlignmentTask(luigi.Task):
             init_frame_height=self.init_frame_height,
             init_focal_length=self.init_focal_length,
         )
+        stereo_fusion_guide = tasks.multiview_stereo.StereoFusionTask(
+            input_path=self.input_path,
+            fps=self.fps,
+            width=self.width,
+            height=self.height,
+            max_keypoints=self.max_keypoints,
+            width_confidence=self.width_confidence,
+            depth_confidence=self.depth_confidence,
+            init_frame_width=self.init_frame_width,
+            init_frame_height=self.init_frame_height,
+            init_focal_length=self.init_focal_length,
+            highres_width=self.highres_width,
+            highres_height=self.highres_height,
+            mask_categories=tuple(utils.object_masking.PLANAR_CATEGORIES),
+        )
         stereo_fusion = tasks.multiview_stereo.StereoFusionTask(
             input_path=self.input_path,
             fps=self.fps,
@@ -82,7 +99,7 @@ class AlignmentTask(luigi.Task):
             highres_height=self.highres_height,
             mask_categories=tuple(utils.object_masking.STATIC_CATEGORIES),
         )
-        return [object_masking, reconstruction, depth, stereo_fusion]
+        return [object_masking, reconstruction, depth, stereo_fusion_guide, stereo_fusion]
 
     def output(self):
         ctx = context.Context()
@@ -91,10 +108,11 @@ class AlignmentTask(luigi.Task):
     def run(self):
         ctx = context.Context()
         with tempfile.TemporaryDirectory() as temp_dir:
-            [[object_masking], [reconstruction], [depth], [stereo_fusion]] = self.input()
+            [[object_masking], [reconstruction], [depth], [planar_stereo_fusion], [stereo_fusion]] = self.input()
             mask_dir = os.path.join(object_masking.read(), "masks")
             model_dir = os.path.join(reconstruction.read(), "model")
             depth_path = os.path.join(depth.read(), "results.npz")
+            fused_guide_path = os.path.join(planar_stereo_fusion.read(), "fused.ply")
             fused_path = os.path.join(stereo_fusion.read(), "fused.ply")
 
             # extract reference model
@@ -104,53 +122,78 @@ class AlignmentTask(luigi.Task):
                 image = model.image(image_id)
                 extrinsics_guide.append(np.concat([image.cam_from_world().matrix(), [[0, 0, 0, 1]]]))
             extrinsics_guide = np.array(extrinsics_guide)
-            # undistort mask
-            mask = utils.alignment.undistort_image_dir(model_dir, mask_dir, rgb=False)
-
             # extract estimated model
             depth_output = np.load(depth_path)
-            image_est = depth_output["image"]
-            depth_est = depth_output["depth"]
-            conf_est = depth_output["conf"]
             extrinsics_est = depth_output["extrinsics"]
-            intrinsics_est = depth_output["intrinsics"]
+            # align model (guided to estimated quantinity)
+            path_guide, path_est, rotate_g2e, translate_g2e, scale_g2e = utils.alignment.align_path_from_extrinsics(
+                extrinsics_guide,
+                extrinsics_est
+            )
+            ctx.logger.info(f"guide to est scale: {scale_g2e}")
+            # debug aligned trajectory
+            fig_path = os.path.join(temp_dir, "trajectory.png")
+            utils.alignment.plot_path(fig_path, path_guide, path_est)
 
-            # check image and mask consistency
-            assert image_est.shape[0] == mask.shape[0], f"image length {image_est.shape[0]} does not match mask length {mask.shape[0]}"
-            # resize mask to match downsampled image for depth estimation
+            # detect plane segment
+            pcd_guide = open3d.io.read_point_cloud(fused_guide_path)
+            pcd_guide.scale(scale_g2e, [0.0, 0.0, 0.0])
+            pcd_guide.rotate(rotate_g2e, center=[0.0, 0.0, 0.0])
+            pcd_guide.translate(translate_g2e)
+            model, _ = pcd_guide.segment_plane(distance_threshold=0.100, ransac_n=3, num_iterations=10000)
+            # align plane normal to z-axis
+            a, b, c, _ = model
+            normal = np.array([a, b, c])
+            normal /= np.linalg.norm(normal)
+            rot, _ = scipy.spatial.transform.Rotation.align_vectors([[0.0, 0.0, 1.0]], [normal])
+            rot_mat = rot.as_matrix()
+            t = -rot_mat @ pcd_guide.get_center()
+            hom_mat = np.eye(4)
+            hom_mat[:3, :3] = rot_mat
+            hom_mat[:3, 3] = t
+
+            # transform reference model
+            pcd_ref = open3d.io.read_point_cloud(fused_path)
+            pcd_ref.scale(scale_g2e, [0.0, 0.0, 0.0])
+            pcd_ref.rotate(rotate_g2e, center=[0.0, 0.0, 0.0])
+            pcd_ref.translate(translate_g2e)
+            pcd_ref.transform(hom_mat)
+
+            # read depth
+            depth = depth_output["depth"]
+            image_est = depth_output["image"]
+            conf_est = depth_output["conf"]
+            intrinsics_est = depth_output["intrinsics"]
+            # read mask
+            mask = utils.alignment.undistort_image_dir(model_dir, mask_dir, rgb=False)
             mask_est = []
             for i in range(mask.shape[0]):
                 m = cv2.resize(mask[i], (image_est.shape[2], image_est.shape[1]), interpolation=cv2.INTER_AREA)
                 mask_est.append(m)
             mask_est = np.array(mask_est)
             mask_est = (mask_est > 127).astype(np.uint8)
-
-            # align model (guided to estimated quantinity)
-            path_guide, path_est, rotate_g2e, translate_g2e, scale_g2e = utils.alignment.align_path_from_extrinsics(extrinsics_guide, extrinsics_est)
-            ctx.logger.info(f"guide to est scale: {scale_g2e}")
-            # debug aligned trajectory
-            fig_path = os.path.join(temp_dir, "trajectory.png")
-            utils.alignment.plot_path(fig_path, path_guide, path_est)
-            # align guide point cloud
-            pcd_guide = open3d.io.read_point_cloud(fused_path)
-            pcd_guide.scale(scale_g2e, [0.0, 0.0, 0.0])
-            pcd_guide.rotate(rotate_g2e, center=[0.0, 0.0, 0.0])
-            pcd_guide.translate(translate_g2e)
-            points_guide = np.asarray(pcd_guide.points)
-
-            # projection dense depth
-            points, colors, rays = utils.alignment.depth_to_world_point(depth_est, intrinsics_est, extrinsics_est, image_est, mask_est, conf_est, self.align_confidence)
-            ctx.logger.info(f"points: {points.shape} {points.dtype}, colors: {colors.shape} {colors.dtype}")
-            # alignment!
-            points, colors, normals = utils.alignment.alignment_model(points, colors, rays, points_guide)
-
-            # write points as PLY format
-            object_path = os.path.join(temp_dir, "object.ply")
+            # projection (z = 0) to plane
+            points, colors = utils.alignment.depth_to_plane(
+                depth,
+                intrinsics_est,
+                extrinsics_est @ np.linalg.inv(hom_mat),
+                image_est,
+                mask_est,
+                conf_est,
+                self.align_confidence
+            )
             pcd_est = open3d.geometry.PointCloud()
             pcd_est.points = open3d.utility.Vector3dVector(points)
             pcd_est.colors = open3d.utility.Vector3dVector(colors.astype(np.float64) / 255.0)
-            pcd_est.normals = open3d.utility.Vector3dVector(normals)
-            pcd = (pcd_est + pcd_guide).voxel_down_sample(voxel_size=self.downsample_resolution)
+
+            # write points as PLY format
+            object_path = os.path.join(temp_dir, "object.ply")
+            pcd = pcd_ref + pcd_est
+            min_bound = np.array([-30.0, -30.0, -30.0])
+            max_bound = np.array([ 30.0,  30.0,  30.0])
+            bbox = o3d.geometry.AxisAlignedBoundingBox(min_bound, max_bound)
+            pcd = pcd.crop(bbox)
+            pcd = pcd.voxel_down_sample(voxel_size=self.downsample_resolution)
             open3d.io.write_point_cloud(object_path, pcd, write_ascii=False, compressed=True)
 
             ctx.logger.info("writing output to database")
--- src/utils/alignment.py
+++ src/utils/alignment.py
@@ -5,95 +5,9 @@ import tempfile
 import cv2
 import evo.core.trajectory
 import evo.tools.plot
-import faiss
 import matplotlib.pyplot as plt
 import numpy as np
 import pycolmap
-import rich.progress
-import torch
-
-import context
-
-
-def align_path_from_extrinsics(
-    extrinsics_target: np.ndarray,
-    extrinsics_ref: np.ndarray,
-) -> tuple[evo.core.trajectory.PosePath3D, evo.core.trajectory.PosePath3D, np.ndarray, np.ndarray, float]:
-    pose_ref = np.linalg.inv(extrinsics_ref)
-    pose_target = np.linalg.inv(extrinsics_target)
-    path_ref = evo.core.trajectory.PosePath3D(poses_se3=pose_ref)
-    path_target = evo.core.trajectory.PosePath3D(poses_se3=pose_target)
-    rotate, translate, scale = path_target.align(path_ref, correct_scale=True)
-    return path_target, path_ref, rotate, translate, scale
-
-
-def plot_path(
-    path: str,
-    path_target: evo.core.trajectory.PosePath3D,
-    path_ref: evo.core.trajectory.PosePath3D
-):
-    fig = plt.figure(figsize=(8, 8))
-    ax = evo.tools.plot.prepare_axis(fig, plot_mode=evo.tools.plot.PlotMode.xyz)
-    evo.tools.plot.traj(ax=ax, plot_mode=evo.tools.plot.PlotMode.xyz, traj=path_target, color="blue")
-    evo.tools.plot.traj(ax=ax, plot_mode=evo.tools.plot.PlotMode.xyz, traj=path_ref, color="red")
-    fig.axes.append(ax)
-    fig.savefig(path)
-
-
-# https://github.com/ByteDance-Seed/Depth-Anything-3/blob/main/src/depth_anything_3/utils/export/glb.py#L205
-def depth_to_world_point(
-    depth: np.ndarray,
-    K: np.ndarray,
-    ext_w2c: np.ndarray,
-    image: np.ndarray,
-    mask: np.ndarray,
-    conf: np.ndarray,
-    conf_thr: float,
-    near_clip: float = 1.0,
-    far_clip: float = 100.0,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    N, H, W = depth.shape
-    us, vs = np.meshgrid(np.arange(W), np.arange(H))
-    ones = np.ones_like(us)
-    pix = np.stack([us, vs, ones], axis=-1).reshape(-1, 3)  # (u, v, 1.0) in (H * W, 3)
-
-    points_all, colors_all, ray_all = [], [], []
-    for i in range(N):
-        d = depth[i]  # depth value in (H, W)
-        valid = np.isfinite(d) & (d > 0)  # boolean in (H, W)
-        valid &= conf[i] >= conf_thr
-        valid &= mask[i] > 0
-        if not np.any(valid):
-            continue
-
-        d_flat = d.reshape(-1)  # depth value in (H * W)
-        valid_idx = np.flatnonzero(valid.reshape(-1))  # int in (H, W)
-
-        K_inv = np.linalg.inv(K[i])  # float in (3, 3) intrisics
-        c2w = np.linalg.inv(ext_w2c[i])  # float in (4, 4) camera to world
-
-        rays = K_inv @ pix[valid_idx].T  # (x, y, z) in (3, M) perspective rays direction
-        pts_c = rays * d_flat[valid_idx][np.newaxis, :]  # (3, M) rays
-        pts_c_hom = np.vstack([pts_c, np.ones((1, pts_c.shape[1]))])
-        pts_w = (c2w @ pts_c_hom)[:3].T.astype(np.float32)  # (M, 3) points
-        colors = image[i].reshape(-1, 3)[valid_idx].astype(np.uint8)  # (M, 3) colors
-
-        # ray (original code)
-        ray_n = rays * near_clip
-        ray_f = rays * far_clip
-        ray_n_hom = np.vstack([ray_n, np.ones((1, ray_n.shape[1]))])
-        ray_f_hom = np.vstack([ray_f, np.ones((1, ray_f.shape[1]))])
-        ray_n_w = (c2w @ ray_n_hom)[:3].T.astype(np.float32)  # (M, 3) points
-        ray_f_w = (c2w @ ray_f_hom)[:3].T.astype(np.float32)  # (M, 3) points
-
-        points_all.append(pts_w)  # (M, 3)
-        colors_all.append(colors)  # (M, 3)
-        ray_all.append(np.stack([ray_n_w, ray_f_w], axis=1))  # (M, 2, 3)
-
-    if len(points_all) == 0:
-        return np.zeros((0, 3), dtype=np.float32), np.zeros((0, 3), dtype=np.uint8), np.zeros((0, 2, 3), dtype=np.uint8)
-
-    return np.concatenate(points_all, 0), np.concatenate(colors_all, 0), np.concatenate(ray_all, 0)
 
 
 def undistort_image_dir(model_dir: str, image_dir: str, rgb: bool) -> np.ndarray:
@@ -123,68 +37,67 @@ def undistort_image_dir(model_dir: str, image_dir: str, rgb: bool) -> np.ndarray
         return images
 
 
-# points align planar and guided points.
-def alignment_model(
-    xyz: np.ndarray,  # (N, 3)
-    rgb: np.ndarray,  # (N, 3)
-    ray: np.ndarray,  # (N, 2, 3)
-    xyz_guide: np.ndarray,  # (N, 3)
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    ctx = context.Context()
-    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def align_path_from_extrinsics(
+    extrinsics_target: np.ndarray,
+    extrinsics_ref: np.ndarray,
+) -> tuple[evo.core.trajectory.PosePath3D, evo.core.trajectory.PosePath3D, np.ndarray, np.ndarray, float]:
+    pose_ref = np.linalg.inv(extrinsics_ref)
+    pose_target = np.linalg.inv(extrinsics_target)
+    path_ref = evo.core.trajectory.PosePath3D(poses_se3=pose_ref)
+    path_target = evo.core.trajectory.PosePath3D(poses_se3=pose_target)
+    rotate, translate, scale = path_target.align(path_ref, correct_scale=True)
+    return path_target, path_ref, rotate, translate, scale
 
-    assert xyz.shape[0] == rgb.shape[0] == ray.shape[0]
-    assert xyz.shape[1] == 3 and rgb.shape[1] == 3 and (ray.shape[1] == 2 and ray.shape[2] == 3)
-    assert xyz.dtype == np.float32 and rgb.dtype == np.uint8 and ray.dtype == np.float32
 
-    N, _ = xyz.shape
-    ctx.logger.info(f"alignment: {N} points.")
+def plot_path(
+    path: str,
+    path_target: evo.core.trajectory.PosePath3D,
+    path_ref: evo.core.trajectory.PosePath3D
+):
+    fig = plt.figure(figsize=(8, 8))
+    ax = evo.tools.plot.prepare_axis(fig, plot_mode=evo.tools.plot.PlotMode.xyz)
+    evo.tools.plot.traj(ax=ax, plot_mode=evo.tools.plot.PlotMode.xyz, traj=path_target, color="blue")
+    evo.tools.plot.traj(ax=ax, plot_mode=evo.tools.plot.PlotMode.xyz, traj=path_ref, color="red")
+    fig.axes.append(ax)
+    fig.savefig(path)
 
-    # build faiss index
-    res = faiss.StandardGpuResources()
-    index_self = faiss.index_cpu_to_gpu(res, 0, faiss.IndexFlatL2(3))
-    index_self.add(xyz)
 
-    # find self nearest neighbors
-    K = 256
-    _, net_self = index_self.search(xyz, K)  # (N, K)
-    net_self = torch.from_numpy(net_self).long().to(dev)  # (N, K)
-    ctx.logger.info(f"computed nearest neighbors: {K} points.")
-    del index_self, res
+def depth_to_plane(
+    depth: np.ndarray,
+    K: np.ndarray,
+    ext_w2c: np.ndarray,
+    image: np.ndarray,
+    mask: np.ndarray,
+    conf: np.ndarray,
+    conf_thr: float,
+) -> tuple[np.ndarray, np.ndarray]:
+    N, H, W, _ = image.shape
+    us, vs = np.meshgrid(np.arange(W), np.arange(H))
+    ones = np.ones_like(us)
+    pix = np.stack([us, vs, ones], axis=-1).reshape(-1, 3)  # (u, v, 1.0) in (H * W, 3)
 
-    # compute ray directions
-    raydirs = ray[:, 1] - ray[:, 0]  # (N, 3)
-    raydirs = raydirs / np.linalg.norm(raydirs, axis=1, keepdims=True)  # (N, 3)
-    raydirs = torch.from_numpy(raydirs).float().to(dev)  # (N, 3)
+    points_all, colors_all = [], []
+    for i in range(N):
+        valid = (depth[i] < 10.0) & (depth[i] > 0)  # boolean in (H, W)
+        valid &= conf[i] >= conf_thr
+        valid &= mask[i] > 0
+        if not np.any(valid):
+            continue
+        valid_idx = np.flatnonzero(valid.reshape(-1))  # int in (H, W)
 
-    EPOCH, ALPHA, B = 32, -0.4, 4096
-    normals = torch.zeros((N, 3), dtype=torch.float32, device=dev)  # (N, 3)
-    current_xyz = torch.from_numpy(xyz).float().to(dev)  # (N, 3)
-    for epoch in rich.progress.track(range(EPOCH), description="alignment model", console=ctx.console):
-        neighbors = current_xyz[net_self]  # (N, K, 3)
+        K_inv = np.linalg.inv(K[i])  # float in (3, 3) intrisics
+        c2w = np.linalg.inv(ext_w2c[i])  # float in (4, 4) camera to world
 
-        # compute local center and normals
-        means = torch.mean(neighbors, dim=1)  # (N, 3)
-        diff = neighbors - means.unsqueeze(1)  # (N, K, 3)
-        cov = torch.bmm(diff.transpose(1, 2), diff) / (K - 1)  # (N, 3, 3)
-        # batch eigen decomposition for large N
-        normals_list = []
-        for i in range(0, N, B):
-            _, eigenvectors = torch.linalg.eigh(cov[i:i + B])  # (B, 3, 3)
-            normals_list.append(eigenvectors[:, :, 0])  # (B, 3)
-        normals = torch.cat(normals_list, dim=0)  # (N, 3)
+        rays = K_inv @ pix[valid_idx].T  # (x, y, z) in (3, M) perspective rays direction
+        camera_w, rays_w = c2w[:3, 3], c2w[:3, :3] @ rays  # (3, M) perspective rays direction in world
+        d = -camera_w[2] / rays_w[2, :]  # (M,) depth along the rays
+        pts_w = (camera_w[:, np.newaxis] + d[np.newaxis, :] * rays_w).T  # (M, 3) points
+        colors = image[i].reshape(-1, 3)[valid_idx]  # (M, 3) colors
 
-        normals = torch.where(normals[:, [1]] > 0, -normals, normals)
+        points_all.append(pts_w)  # (M, 3)
+        colors_all.append(colors)  # (M, 3)
 
-        # compute patch (local plane)
-        n_dot_r = torch.sum(normals * raydirs, dim=1, keepdims=True)  # (N, 1)
-        d_dot_n = torch.sum((current_xyz - means) * normals, dim=1, keepdims=True)  # (N, 1)
-        patch_term = ALPHA * d_dot_n * n_dot_r  # (N, 1)
-        ctx.logger.info(f"[{1 + epoch}/{EPOCH}] displacement error: {torch.mean(patch_term)}")
+    if len(points_all) == 0:
+        return np.zeros((0, 3), dtype=np.float32), np.zeros((0, 3), dtype=np.uint8)
 
-        # apply
-        current_xyz = current_xyz + patch_term * raydirs  # (N, 3)
-    xyz = current_xyz.cpu().numpy()
-    normals = normals.cpu().numpy()
-
-    return xyz, rgb, normals
+    return np.concatenate(points_all, 0), np.concatenate(colors_all, 0)
